bbf379f7842113543f763b1183ba1303fbdc4125
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-12 13:28:51 +0300

    init

---
+++ ChicknMatch/App/ChicknMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicknMatchApp.swift
+//  ChicknMatch
+//
+//  Created by Serhii Babchuk on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChicknMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CMLaunchView()
+        }
+    }
+}
