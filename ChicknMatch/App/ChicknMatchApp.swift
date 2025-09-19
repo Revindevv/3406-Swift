98eff973e0f71a60d471ecddea2e28d966008ca4
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-19 12:40:40 +0300

    add grey part app

--- ChicknMatch/App/ChicknMatchApp.swift
+++ ChicknMatch/App/ChicknMatchApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ChicknMatchApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            CMLaunchView()
+            AppEntryPoint()
         }
     }
 }
